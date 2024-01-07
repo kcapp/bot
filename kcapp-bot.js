20e92f840aeb24f6e25b2747be6ad2b4037de40c
lluni <36239317+lluni@users.noreply.github.com>
2024-01-07 02:19:48 +0100

    Added support for multiple simultaneous bot instances

--- kcapp-bot.js
+++ kcapp-bot.js
@@ -2,79 +2,89 @@ const debug = require('debug')('kcapp-bot:main');
 const decache = require('decache');
 const sleep = require('./sleep');
 
-let firstThrow = true;
-
-async function doScore(socket, bot) {
-    const player = socket.currentPlayer;
-    if (player.player_id === bot.id) {
-        if (firstThrow) {
-            debug(`Waiting because of first throw`);
-            await sleep(5000); // wait for page to load
-        }
-        await sleep(500);
-        await bot.score(socket);
-        await sleep(1000);
-        socket.emitVisit();
+class KcappBot {
+    constructor(botId, sioURL, sioPort = 3000, apiURL = 'http://localhost:8001', protocol = 'http') {
+        this.botId = botId;
+        this.sioURL = sioURL;
+        this.sioPort = sioPort;
+        this.apiURL = apiURL;
+        this.protocol = protocol;
+        this.firstThrow = true;
     }
-    firstThrow = false;
+
+    async doScore(socket, bot) {
+        const player = socket.currentPlayer;
+        if (player.player_id === bot.id) {
+            if (this.firstThrow) {
+                debug(`Waiting because of first throw`);
+                await sleep(5000); // wait for page to load
+            }
+            await sleep(500);
+            await bot.score(socket);
+            await sleep(1000);
+            socket.emitVisit();
+        }
+        this.firstThrow = false;
+    }
+
+    async handleScoreUpdate(socket, data, bot) {
+        const leg = data.leg;
+        if (leg.is_finished) {
+            return;
+        } else if (leg.current_player_id !== bot.id) {
+            debug(`[${leg.id}] Not our turn, waiting...`);
+        } else if (data.is_undo) {
+            debug(`[${leg.id}] Recevied undo visit, forwarding`);
+            bot.undoVisit();
+            socket.emit('undo_visit', {});
+        } else {
+            this.doScore(socket, bot);
+        }
+    }
+
+    playLeg(legId, botSkill) {
+        const SkillBot = require('./bot');
+        const bot = new SkillBot(this.botId, botSkill);
+
+        const kcapp = require('kcapp-sio-client/kcapp')(this.sioURL, this.sioPort, 'kcapp-bot', this.protocol);
+         // Make sure we get a separate instance for each leg we connect to...
+        decache('kcapp-sio-client/kcapp');
+        kcapp.connectLegNamespace(legId, (socket) => {
+            debug(`[${legId}] kcapp-bot connected to leg`);
+            this.firstThrow = true;
+
+            socket.on('score_update', (data) => {
+                this.handleScoreUpdate(socket, data, bot);
+            });
+            socket.on('leg_finished', (data) => {
+                debug(`[${legId}] Leg is finished`);
+                socket.disconnect();
+            });
+            this.doScore(socket, bot);
+        });
+    }
+
+    replayLeg(legId, playerId, startingScore = 301) {
+        const ReplayBot = require('./replay-bot')
+        const bot = new ReplayBot(this.botId, legId, playerId, this.apiURL, startingScore);
+
+        const kcapp = require('kcapp-sio-client/kcapp')(this.sioURL, this.sioPort, 'kcapp-bot', this.protocol);
+         // Make sure we get a separate instance for each leg we connect to...
+        decache('kcapp-sio-client/kcapp');
+        kcapp.connectLegNamespace(legId, (socket) => {
+            debug(`[${legId}] replay-bot connected to leg`);
+            this.firstThrow = true;
+
+            socket.on('score_update', (data) => {
+                this.handleScoreUpdate(socket, data, bot);
+            });
+            socket.on('leg_finished', (data) => {
+                debug(`[${legId}] Leg is finished`);
+                socket.disconnect();
+            });
+            this.doScore(socket, bot);
+        });
+    }
 }
 
-async function handleScoreUpdate(socket, data, bot) {
-    const leg = data.leg;
-    if (leg.is_finished) {
-        return;
-    } else if (leg.current_player_id !== bot.id) {
-        debug(`[${leg.id}] Not our turn, waiting...`);
-    } else if (data.is_undo) {
-        debug(`[${leg.id}] Recevied undo visit, forwarding`);
-        bot.undoVisit();
-        socket.emit('undo_visit', {});
-    } else {
-        doScore(socket, bot);
-    }
-}
-
-module.exports = (botId, sioURL, sioPort = 3000, apiURL = 'http://localhost:8001', protocol = 'http') => {
-    return {
-        playLeg: (legId, botSkill) => {
-            const bot = require('./bot')(botId, botSkill);
-
-            const kcapp = require('kcapp-sio-client/kcapp')(sioURL, sioPort, 'kcapp-bot', protocol);
-             // Make sure we get a separate instance for each leg we connect to...
-            decache('kcapp-sio-client/kcapp');
-            kcapp.connectLegNamespace(legId, (socket) => {
-                debug(`[${legId}] kcapp-bot connected to leg`);
-                firstThrow = true;
-
-                socket.on('score_update', (data) => {
-                    handleScoreUpdate(socket, data, bot);
-                });
-                socket.on('leg_finished', (data) => {
-                    debug(`[${legId}] Leg is finished`);
-                    socket.disconnect();
-                });
-                doScore(socket, bot);
-            });
-        },
-        replayLeg: (legId, playerId, startingScore = 301) => {
-            const bot = require('./replay-bot')(botId, legId, playerId, apiURL, startingScore);
-
-            const kcapp = require('kcapp-sio-client/kcapp')(sioURL, sioPort, 'kcapp-bot', protocol);
-             // Make sure we get a separate instance for each leg we connect to...
-            decache('kcapp-sio-client/kcapp');
-            kcapp.connectLegNamespace(legId, (socket) => {
-                debug(`[${legId}] replay-bot connected to leg`);
-                firstThrow = true;
-
-                socket.on('score_update', (data) => {
-                    handleScoreUpdate(socket, data, bot);
-                });
-                socket.on('leg_finished', (data) => {
-                    debug(`[${legId}] Leg is finished`);
-                    socket.disconnect();
-                });
-                doScore(socket, bot);
-            });
-        }
-    };
-};
+module.exports = KcappBot;
--- replay-bot.js
+++ replay-bot.js
@@ -2,90 +2,94 @@ const axios = require('axios');
 const debug = require('debug')('kcapp-bot:replay-bot');
 const sleep = require('./sleep');
 
-/**
- * Create a new replay-bot which will replay a random leg for the given player
- * @param {int} - PlayerID of the given player
- * @param {string} - Base URL of kcapp API
- * @param {int} - Starting score of the leg
- */
-exports.setup = (playerId, legId, apiURL, startingScore) => {
-    debug(`[${legId}] Requesting leg to replay for ${playerId}`)
-    axios.get(`${apiURL}/player/${playerId}/random/${startingScore}`)
-        .then(response => {
-            const visits = response.data;
-            this.visits = visits;
-            debug(`[${legId}] Configured bot for leg ${visits[0].leg_id}`);
-        }).catch(error => {
-            debug(`Error when getting match: ${error}`);
-        });
-}
-
-/**
- * Get the next visit to use for scoring
- */
-exports.getVisit = () => {
-    const visit = this.visits.shift();
-    this.visitsThrown.push(visit);
-    return visit;
-}
-
-/**
- * Undo the previous visit
- */
-exports.undoVisit = () => {
-    const visit = this.visitsThrown.pop()
-    this.visits.unshift(visit);
-}
-
-/**
- * Get the correct dart to throw
- * @param {object} - Visit
- * @param {int} - Number of darts thrown
- */
-exports.attemptThrow = (visit, dartsThrown) => {
-    let dart = { score: visit.first_dart.value, multiplier: visit.first_dart.multiplier };
-    if (dartsThrown === 1) {
-        dart = { score: visit.second_dart.value, multiplier: visit.second_dart.multiplier };
-    } else if (dartsThrown == 2) {
-        dart = { score: visit.third_dart.value, multiplier: visit.third_dart.multiplier };
+class Bot {
+    /**
+     * @param {int} - Bot ID
+     * @param {int} - Leg ID
+     * @param {int} - Player ID
+     * @param {string} - kcapp API URL
+     * @param {int} - Starting score of leg
+     */
+    constructor(id, legId, playerId, apiURL, startingScore) {
+        this.id = id;
+        this.legId = legId;
+        this.setup(playerId, legId, apiURL, startingScore);
+        this.visits = [];
+        this.visitsThrown = [];
+        return this;
     }
-    debug(`[${this.legId}] Throw ${JSON.stringify(dart)}`);
-    return dart;
-}
 
-/**
- * Score a visit
- * @param {object} - Socket for scoring
- */
-exports.score = async (socket) => {
-    const visit = this.getVisit();
-    const first = this.attemptThrow(visit, 0);
-    socket.emitThrow(first);
-    await sleep(100);
-    const second = this.attemptThrow(visit, 1);
-    if (second.score) {
-        socket.emitThrow(second);
+    /**
+     * Create a new replay-bot which will replay a random leg for the given player
+     * @param {int} - PlayerID of the given player
+     * @param {string} - Base URL of kcapp API
+     * @param {int} - Starting score of the leg
+     */
+    setup(playerId, legId, apiURL, startingScore) {
+        debug(`[${legId}] Requesting leg to replay for ${playerId}`)
+        axios.get(`${apiURL}/player/${playerId}/random/${startingScore}`)
+            .then(response => {
+                const visits = response.data;
+                this.visits = visits;
+                debug(`[${legId}] Configured bot for leg ${visits[0].leg_id}`);
+            }).catch(error => {
+                debug(`Error when getting match: ${error}`);
+            });
+    }
+
+    /**
+     * Get the next visit to use for scoring
+     */
+    getVisit() {
+        const visit = this.visits.shift();
+        this.visitsThrown.push(visit);
+        return visit;
+    }
+
+    /**
+     * Undo the previous visit
+     */
+    undoVisit() {
+        const visit = this.visitsThrown.pop()
+        this.visits.unshift(visit);
+    }
+
+    /**
+     * Get the correct dart to throw
+     * @param {object} - Visit
+     * @param {int} - Number of darts thrown
+     */
+    attemptThrow(visit, dartsThrown) {
+        let dart = { score: visit.first_dart.value, multiplier: visit.first_dart.multiplier };
+        if (dartsThrown === 1) {
+            dart = { score: visit.second_dart.value, multiplier: visit.second_dart.multiplier };
+        } else if (dartsThrown == 2) {
+            dart = { score: visit.third_dart.value, multiplier: visit.third_dart.multiplier };
+        }
+        debug(`[${this.legId}] Throw ${JSON.stringify(dart)}`);
+        return dart;
+    }
+
+    /**
+     * Score a visit
+     * @param {object} - Socket for scoring
+     */
+    async score(socket) {
+        const visit = this.getVisit();
+        const first = this.attemptThrow(visit, 0);
+        socket.emitThrow(first);
         await sleep(100);
-    }
-    const third = this.attemptThrow(visit, 2);
-    if (third.score) {
-        socket.emitThrow(third);
-        await sleep(100);
+        const second = this.attemptThrow(visit, 1);
+        if (second.score) {
+            socket.emitThrow(second);
+            await sleep(100);
+        }
+        const third = this.attemptThrow(visit, 2);
+        if (third.score) {
+            socket.emitThrow(third);
+            await sleep(100);
+        }
     }
 }
 
-/**
- * @param {int} - Bot ID
- * @param {int} - Leg ID
- * @param {int} - Player ID
- * @param {string} - kcapp API URL
- * @param {int} - Starting score of leg
- */
-module.exports = (id, legId, playerId, apiURL, startingScore) => {
-    this.id = id;
-    this.legId = legId;
-    this.setup(playerId, legId, apiURL, startingScore);
-    this.visits = [];
-    this.visitsThrown = [];
-    return this;
-}
+module.exports = Bot;
